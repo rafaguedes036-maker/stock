a828c48f460f6330514a78ca571d7ba4bc9f8ce9
Rafael Guedes <rafaguedes036@gmail.com>
2026-03-12 00:31:13 +0000

    Atualização

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from functions import insert_product, list_products, delete_product, update_prod
 
 
 st.set_page_config(page_title= 'Sistema de Estoque', layout= 'centered')
-st.title('Sistema de Estoque')
+st.title('📦Sistema de Estoque')
 
 menu = st.sidebar.selectbox("Menu", ["Inserir Produto", "Ver Produtos", "Atualizar Produto", "Deletar Produto"])
 
 #Inserir Produto
 
 if menu == "Inserir Produto":
-    st.subheader("Inserir Produto")
+    st.subheader("➕Inserir Produto")
     name = st.text_input("Nome do Produto")
     category = st.text_input("Categoria")
     price = st.number_input("Preço do Produto", min_value=0.0, format="%.2f")
@@ -24,23 +24,23 @@ if menu == "Inserir Produto":
     if st.button("Salvar"):
         if name:
             insert_product(name, category, price, quitantity, provider)
-            st.success("Produto inserido com sucesso!")
+            st.success("✅ Produto inserido com sucesso!")
         
         else:
-            st.error("O nome do produto é obrigatório.")
+            st.error("⚠️ O nome do produto é obrigatório.")
 
 
 #Vizualizar Produtos
 
 elif menu == "Ver Produtos":
-    st.subheader("Todos os produtos do estoque ")
+    st.subheader("📋Todos os produtos do estoque ")
     dados = list_products()
     st.dataframe(dados, use_container_width=True)
 
 #Atualizar Produto
 
 elif menu == "Atualizar Produto":
-    st.subheader("Atualizar Produto")
+    st.subheader("✏️Atualizar Produto")
     dados = list_products()
     produtos = {f"{p[0]} - {p[1]}": p for p in dados}
     escolhido = st.selectbox("Selecione o produto para atulizar", list(produtos.keys()))
@@ -54,16 +54,16 @@ elif menu == "Atualizar Produto":
 
     if st.button("Salvar alterações"):
         update_product(p[0], novo_nome, nova_categoria, novo_preco, nova_quantidade, novo_fornecedor)
-        st.success("Produto atualizado com sucesso!")
+        st.success("✅ Produto atualizado com sucesso!")
 
 
 #Deletar Produto
 elif menu == "Deletar Produto":
-    st.subheader("Deletar Produto")
+    st.subheader("🗑️Deletar Produto")
     dados = list_products()
     produtos = {f"{p[0]} - {p[1]}": p[0] for p in dados}
     escolhido = st.selectbox("Selecione o produto para deletar", list(produtos.keys()))
     if st.button("Deletar"):
         delete_product(produtos[escolhido])
-        st.success("Produto deletado com sucesso!")
+        st.success("✅ Produto deletado com sucesso!")
 
